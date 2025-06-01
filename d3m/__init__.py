95b704955de596de8f25f882c773db669127ddef
Alejandro Buendia <alb2281@columbia.edu>
2025-05-31 19:46:16 -0700

    Update install

--- d3m/__init__.py
+++ d3m/__init__.py
@@ -1,6 +1,6 @@
 from .attrib import get_trak_matrix
-from .dda import DDA
-from .autodda import AutoDDA
+from .d3m import D3M
+from .autod3m import AutoDDA
 
 __version__ = "1.0.0"
 VERSION = __version__
